01293a2944677444b9e789cd3a9a10f8022858d9
JN J <jjnlq@tju.edu.cn>
2023-10-21 12:20:19 +0800

    现行版本

--- sql/sql_now/pharmacy_administrator.sql
+++ sql/sql_now/pharmacy_administrator.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `pharmacy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pharmacy`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pharmacy
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-21 11:44:21
+-- Dump completed on 2023-10-21 12:19:26
