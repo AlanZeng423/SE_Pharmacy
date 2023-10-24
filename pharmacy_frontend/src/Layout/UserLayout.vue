3b23a501f0a202a8aab983a6b3c466d3446e9bca
Yhy <yinhongyang546@gmail.com>
2023-10-24 19:29:14 +0800

    病人两个页面

--- pharmacy_frontend/src/Layout/UserLayout.vue
+++ pharmacy_frontend/src/Layout/UserLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="UserLayout">
-    <a-layout style="height: 400px">
+    <a-layout style="height: 100vh">
       <a-layout-header class="header">
         <GlobalHeader />
       </a-layout-header>
